62623e4c09ca4a6ccc3fc08a79f8af041cd80cf6
MalikiBrahim1 <e41210698@student.polije.ac.id>
2022-02-14 16:58:57 +0700

    Create UASpemdas.java

---
+++ Minggu1/UASpemdas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uaspemdas;
+
+/**
+ *
+ * @author SAMSUNG
+ */
+public class UASpemdas {
+
+    public static void main(String[] args) {
+         System.out.println("===UAS PEMROGAMAN DASAR===\n");
+         System.out.println("Hello Kelas");
+         System.out.println("Nama Saya MUHAMMAD MAULANA MALIK IBRAHIM");
+    }   
+}
